488e1705daeb25759ed428b2eb1212d32d7c67f8
Matt Magurany/n <honkywater@gmail.com>
2012-09-10 23:57:41 -0400

    Added an ASP.NET MVC 4/Web API project for testing and code examples.

---
+++ src/RegexRoutingExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RegexRoutingExample.MvcApplication" Language="C#" %>
---
+++ src/RegexRoutingExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Regular Expression Routing Example Project")]
+[assembly: AssemblyDescription("Example project to test and demonstrate the usage of regular expression routing.")]
