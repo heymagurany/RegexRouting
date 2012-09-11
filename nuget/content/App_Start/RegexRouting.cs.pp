fc342879a82baca3cbd7b53144a01068db2ccbfd
Matt Magurany/n <honkywater@gmail.com>
2012-09-10 23:58:06 -0400

    Build out of NuGet package files.

---
+++ nuget/content/App_Start/RegexRouting.cs.pp
@@ -0,0 +1,16 @@
+﻿using System;
+using WebActivator;
+
+[assembly: PreApplicationStartMethod(typeof($rootnamespace$.App_Start.RegexRouting), "PreStart")]
+
+namespace $rootnamespace$.App_Start
+{
+    public static class RegexRouting
+	{
+        public static void PreStart()
+		{
+			RegexRouteConfig.EnableRegexHttpRoutes(GlobalConfiguration.Configuration);
+			RegexRouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
---
+++ nuget/content/App_Start/RegexRoutingConfig.cs.pp
@@ -0,0 +1,28 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Magurany.Web.Routing.RegularExpressions;
+
+namespace $rootnamespace$
+{
+	public class RegexRouteConfig
+	{
+		public static void EnableRegexHttpRoutes(HttpConfiguration configuration)
+		{
+			RegexRouteHandler messageHandler = new RegexRouteHandler(configuration);
+
+			configuration.MessageHandlers.Add(messageHandler);
+		}
+
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			);
+		}
+	}
+}
