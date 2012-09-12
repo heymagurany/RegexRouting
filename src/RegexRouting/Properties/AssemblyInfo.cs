cc25fe26e0538807da84821bd362124ca1eb8300
Matt Magurany/n <honkywater@gmail.com>
2012-09-11 23:34:35 -0400

    More build out of the nuget package

--- src/RegexRouting/Properties/AssemblyInfo.cs
+++ src/RegexRouting/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("Regular Expression Routing Core")]
+[assembly: AssemblyTitle("Regular Expression Routing")]
 [assembly: AssemblyDescription("Define your routes using regular expressions in ASP.NET MVC and Web API")]
